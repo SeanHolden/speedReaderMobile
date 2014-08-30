3fcdd9a9951408c521a3207c5f6d2fd26878a4aa
Sean Holden <seanholden1@gmail.com>
2014-08-30 16:48:26 +0100

    initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, SpeedReadText) {
+  $scope.textString = SpeedReadText.get();
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('AccountCtrl', function($scope) {
+})
+
+.controller('PasteTextCtrl', function($scope, SpeedReadText){
+  $scope.textString = SpeedReadText.get();
+  $scope.snippet = $scope.textString.value;
+
+  this.saveText = function(newText){
+    oldText = SpeedReadText.get();
+    oldText.value = newText;
+  }
+});
